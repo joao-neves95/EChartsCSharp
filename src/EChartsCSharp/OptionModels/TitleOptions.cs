6bea70587bfd5096caafb9db92264de3a038d715
João Neves <joao95neves@gmail.com>
2021-04-21 18:58:57 +0100

    [EChartsCSharp]: Added the Title options and extensions

---
+++ src/EChartsCSharp/OptionModels/TitleOptions.cs
@@ -0,0 +1,15 @@
+﻿
+namespace EChartsCSharp.OptionModels
+{
+    public class TitleOptions
+    {
+        public  string Title { get; private set; } = "";
+
+        public TitleOptions SetTitle(string title)
+        {
+            this.Title = title;
+
+            return this;
+        }
+    }
+}
